889187f6508607792dbf189ec9952de88b95da12
fygethub <123456.zxc>
2016-04-08 09:12:43 +0800

    activiti
    activity建表以后

---
+++ src/test/java/junt/TestActivity.java
@@ -0,0 +1,31 @@
+package junt;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+public class TestActivity {
+	 /*使用代码创建工作流需要的23张表*/
+	
+	/*@Test
+	public void create(){
+		ProcessEngineConfiguration processEngineConfiguration=ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+		processEngineConfiguration.setJdbcDriver("com.mysql.jdbc.Driver");
+		processEngineConfiguration.setJdbcUrl("jdbc:mysql://localhost:3306/sypro?userUnicode=true&characterEncoding-utf8");
+		processEngineConfiguration.setJdbcUsername("root");
+		processEngineConfiguration.setJdbcPassword("123456");
+		processEngineConfiguration.setDatabaseSchemaUpdate(processEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+		
+		ProcessEngine processEngine=processEngineConfiguration.buildProcessEngine();
+		System.out.println("processEngine"+processEngine);
+	}
+	*/
+	@Test
+	public void create2(){
+		//ProcessEngineConfiguration processEngineConfiguration=ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti-cfg.xml");
+		//ProcessEngine processEngine=processEngineConfiguration.buildProcessEngine();
+		ProcessEngine processEngine=ProcessEngines.getDefaultProcessEngine();
+		
+		System.out.println(processEngine);
+	}
+}
